72596f964f93f7073e3854dcd248cc530ed2376c
Python_Max <python.max@gmail.com>
2016-05-24 10:45:42 +0300

    * use pre-built binary instead of building from source + add user & group in deb postinst - LOG_DIR & LOG4J config file location

--- make_deb.sh
+++ make_deb.sh
@@ -2,22 +2,25 @@
 # 2015-Mar-18 Updated to latest Kafka stable: 0.8.2.1
 set -e
 set -u
+
+version=0.9.0.1
+scala_version=2.11
+
 name=kafka
-version=0.8.2.1
 description="Apache Kafka is a distributed publish-subscribe messaging system."
 url="https://kafka.apache.org/"
 arch="all"
 section="misc"
 license="Apache Software License 2.0"
 package_version="-1"
-src_package="kafka-${version}-src.tgz"
-download_url="http://mirror.sdunix.com/apache/kafka/${version}/${src_package}"
+bin_package="kafka_${scala_version}-${version}.tgz"
+bin_download_url="http://apache.ip-connect.vn.ua/kafka/${version}/${bin_package}"
 origdir="$(pwd)"
 
 #_ MAIN _#
 rm -rf ${name}*.deb
-if [[ ! -f "${src_package}" ]]; then
-  wget ${download_url}
+if [[ ! -f "${bin_package}" ]]; then
+  wget -c ${bin_download_url}
 fi
 mkdir -p tmp && pushd tmp
 rm -rf kafka
@@ -27,15 +30,16 @@ mkdir -p build/usr/lib/kafka
 mkdir -p build/etc/default
 mkdir -p build/etc/init
 mkdir -p build/etc/kafka
+mkdir -p build/var/lib/kafka
 mkdir -p build/var/log/kafka
 
-cp ${origdir}/kafka-broker.default build/etc/default/kafka-broker
-cp ${origdir}/kafka-broker.upstart.conf build/etc/init/kafka-broker.conf
+cp ${origdir}/kafka.default build/etc/default/kafka
+cp ${origdir}/kafka.upstart.conf build/etc/init/kafka.conf
 
-tar zxf ${origdir}/${src_package}
-cd kafka-${version}-src
-sbt update
-sbt package
+tar zxf ${origdir}/${bin_package}
+cd kafka_${scala_version}-${version}
+
+sed -i 's/\/tmp\/kafka-logs/\/var\/lib\/kafka/g' config/server.properties
 mv config/log4j.properties config/server.properties ../build/etc/kafka
 mv * ../build/usr/lib/kafka
 cd ../build
@@ -51,6 +55,7 @@ fpm -t deb \
     --license "${license}" \
     -m "${USER}@localhost" \
     --prefix=/ \
+    --after-install ${origdir}/kafka.postinst \
     -s dir \
     -- .
 mv kafka*.deb ${origdir}
